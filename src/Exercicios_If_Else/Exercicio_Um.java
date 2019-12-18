f84dfa6fe96cd3f329f00007d21ad63eb5bf793d
Igor Oliveira <igoortech@gmail.com>
2019-12-18 19:00:43 -0300

    Iniciei e Salvei no Github

---
+++ src/Exercicios_If_Else/Exercicio_Um.java
@@ -0,0 +1,22 @@
+package Exercicios_If_Else;
+
+import java.util.Scanner;
+
+public class Exercicio_Um {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//EXERCICIO PARA DESCOBRIR SE O NUMERO
+		//DIGITADO É POSITIVO OU NEGATIVO
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("DIGITE UM NÚMERO INTEIRO");
+		int x = sc.nextInt();
+		
+		
+		
+		
+	}
+
+}
