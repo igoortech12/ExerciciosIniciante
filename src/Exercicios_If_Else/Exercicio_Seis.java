e06de62b24a3459a982220f43fa420c5a633f380
Igor Oliveira <igoortech@gmail.com>
2019-12-19 20:11:13 -0300

    Exercicio6 iniciado

---
+++ src/Exercicios_If_Else/Exercicio_Seis.java
@@ -0,0 +1,20 @@
+package Exercicios_If_Else;
+
+import java.util.Scanner;
+
+public class Exercicio_Seis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// LER VALOR QUALQUER E APRESENTAR UMA MSG DIZENDO EM QUAL DOS
+		//SEGUINTES INTERVALOS ([0,25], (25,50], (50,75], (75,100]) O
+		//VALOR SE ENCONTRA.
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		sc.close();
+	}
+
+}
