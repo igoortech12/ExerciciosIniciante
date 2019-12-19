6d59a974adbe9f690c0012e12b1a62c6b04d70ff
Igor Oliveira <igoortech@gmail.com>
2019-12-19 14:17:16 -0300

    Iniciando Exercicio4

---
+++ src/Exercicios_If_Else/Exercicio_Quatro.java
@@ -0,0 +1,12 @@
+package Exercicios_If_Else;
+
+public class Exercicio_Quatro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// LER HORÁRIO DE INÍCIO E TERMINO DE UM JOGO PARA DEPOIS 
+		//CALCULAR A DURAÇÃO DO JOGO.
+		
+	}
+
+}
