5ca592ea7a993f4e799fa1cfd21d7d2f8357ed12
Igor Oliveira <igoortech@gmail.com>
2019-12-19 20:46:35 -0300

    Exercicio5 concluido

--- src/Exercicios_If_Else/Exercicio_Seis.java
+++ src/Exercicios_If_Else/Exercicio_Seis.java
@@ -1,5 +1,6 @@
 package Exercicios_If_Else;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercicio_Seis {
@@ -10,8 +11,27 @@ public class Exercicio_Seis {
 		//SEGUINTES INTERVALOS ([0,25], (25,50], (50,75], (75,100]) O
 		//VALOR SE ENCONTRA.
 		
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+				
+		System.out.println("DIGITE UM VALOR");
+		double valor = sc.nextDouble();
 		
+		if (valor < 0 || valor > 100) {
+			System.out.println("Fora de intervalo");
+		}
+		else if (valor <= 25) {
+			System.out.println("Intervalo [0,25]");
+		}
+		else if (valor <= 50) {
+			System.out.println("Intervalo [25,50]");
+		}
+		else if (valor <= 75) {
+			System.out.println("Intervalo [50,75]");
+		}
+		else if (valor <= 100) {
+			System.out.println("Intervalo [75,100]");
+		}
 		
 		
 		sc.close();
