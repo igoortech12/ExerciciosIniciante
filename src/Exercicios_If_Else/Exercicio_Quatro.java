2ab8119106e178947d9df0f64b0c08a9cfc07d2b
Igor Oliveira <igoortech@gmail.com>
2019-12-19 18:03:11 -0300

    Exercicio4 parcialmente concluído

--- src/Exercicios_If_Else/Exercicio_Quatro.java
+++ src/Exercicios_If_Else/Exercicio_Quatro.java
@@ -1,5 +1,7 @@
 package Exercicios_If_Else;
 
+import java.util.Scanner;
+
 public class Exercicio_Quatro {
 
 	public static void main(String[] args) {
@@ -7,6 +9,22 @@ public class Exercicio_Quatro {
 		// LER HORÁRIO DE INÍCIO E TERMINO DE UM JOGO PARA DEPOIS 
 		//CALCULAR A DURAÇÃO DO JOGO.
 		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("CALCULAR DURAÇÃO DE UM JOGO\n");
+		System.out.println("OBSERVAÇÃO: PADRÃO DE HORÁRIO É DE 24H\n");
+		System.out.println("DIGITE O HORÁRIO DE INÍCIO DO JOGO");
+		int I = sc.nextInt();
+		System.out.println("\nDIGITE O HORÁRIO DO TERMINO DO JOGO");
+		int T = sc.nextInt();
+		
+		if (I <= T) {
+			System.out.println("\nO JOGO DUROU " + (T - I) + " HORA(S).");
+		}
+		else {
+			System.out.println("\nO JOGO DUROU " + (24 - ( I - T)) + " HORA(S).");
+		}
+		sc.close();
 	}
 
 }
