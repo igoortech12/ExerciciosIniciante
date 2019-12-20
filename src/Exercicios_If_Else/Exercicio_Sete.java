72e400de8a6dc9ee649122e7aee8b3b3ef986389
Igor Oliveira <igoortech@gmail.com>
2019-12-19 23:56:00 -0300

    Exercicio7 concluido.

--- src/Exercicios_If_Else/Exercicio_Sete.java
+++ src/Exercicios_If_Else/Exercicio_Sete.java
@@ -13,26 +13,26 @@ public class Exercicio_Sete {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("QUAL QUADRANTE?");
+		System.out.println("----QUAL QUADRANTE?----\n");
 		System.out.println("DIGITE A 1a COORDENADA");
 		double x = sc.nextDouble();
 		System.out.println("\nDIGITE A 2a COORDENADA");
 		double y = sc.nextDouble();
 		
 		if (x > 0 && y > 0) {
-			System.out.println("Q1");
+			System.out.println("\nQUADRANTE 1");
 		}
 		else if (x < 0 && y > 0) {
-			System.out.println("Q2");
+			System.out.println("\nQUADRANTE 2");
 		}
 		else if (x < 0 && y < 0) {
-			System.out.println("Q3");
+			System.out.println("\nQUADRANTE 3");
 		}
 		else if (x > 0 && y < 0) {
-			System.out.println("Q4");
+			System.out.println("\nQUADRANTE 4");
 		}
 		else {
-			System.out.println("ORIGEM");
+			System.out.println("\nORIGEM");
 		}
 		sc.close();
 	}
